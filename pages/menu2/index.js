50fcb77b1974e0d76a4c2243e6081950c05800b4
zhouqian <985428503@qq.com>
2019-07-17 20:15:59 +0800

    takephoto

--- pages/menu2/index.js
+++ pages/menu2/index.js
@@ -1,66 +1,43 @@
-// pages/menu2/index.js
-Page({
-
-  /**
-   * 页面的初始数据
-   */
+var app=getApp()
+Page
+({
   data: {
-
+  navbar:['今日推荐','时尚','国际','美妆','母婴','居家'],
+  currentTab:0,
+    imgUrls: [
+      "/images/201.jpg",
+      "/images/202.jpg",
+      "/images/203.jpg",
+      "/images/204.jpg"
+    ],
+    interval: 1000,
+    duration: 500,
+    changeIndicatorDots: function (e) {
+      this.setData({
+        indicatorDots: !this.data.indicatorDots
+      })
+    },
+    changeAutoplay: function (e) {
+      this.setData({
+        autoplay: !this.data.autoplay
+      })
+    },
+    intervalChange: function (e) {
+      this.setData({
+        interval: e.detail.value
+      })
+    },
+    durationChange: function (e) {
+      this.setData({
+        duration: e.detail.value
+      })
+    },
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+navarTap:function(e)
+{
+  console.debug(e);
+  this,setData({
+  currentTab:e.currentTarget.dataset.idx
+})
+},
 })
